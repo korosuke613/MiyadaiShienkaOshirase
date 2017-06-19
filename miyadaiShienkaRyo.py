4cbd6be44ba657c6476af3115bc8c3abeeda1257
Futa Hirakoba <korosuke613613@gmail.com>
2017-06-20 04:14:58 +0900

    No CSV

--- miyadaiShienkaRyo.py
+++ miyadaiShienkaRyo.py
@@ -2,34 +2,31 @@ import csv
 import urllib.request
 from bs4 import BeautifulSoup
 
-# URLの指定
-html = urllib.request.urlopen("http://gakumu.of.miyazaki-u.ac.jp/gakumu/allnews")
-soup = BeautifulSoup(html, "html.parser")
-
-# テーブルを指定
-ul = soup.findAll('ul', class_='category-module')
-
-csvFile = open("miyadai.csv", 'wt', newline = '', encoding = 'utf-8-sig')
-writer = csv.writer(csvFile)
-
-try:
+def miyadaiOshirase():
+    
+    # URLの指定
+    html = urllib.request.urlopen("http://gakumu.of.miyazaki-u.ac.jp/gakumu/allnews")
+    soup = BeautifulSoup(html, "html.parser")
+    
+    # テーブルを指定
+    ul = soup.findAll('ul', class_='category-module')
+    
+    i = 0
+    
     for li_month in ul[0].findAll('li'):
-        for month in li_month.findAll('h3'):
-            if month.string is not None:
-                csvRow = []
-                writer.writerow(csvRow)
-                csvRow = []
-                csvRow.append(month.string)
-                writer.writerow(csvRow)
         for li in li_month.findAll('li'):
-            csvRow = []
             for span in li.findAll('span'):
                 if span.string is not None:
-                    csvRow.append(span.string)
+                    print(span.string)
             for a in li.findAll('a'):
                 if a.string is not None:
-                    csvRow.append(a.string)
-                    csvRow.append('http:/' + a.get('href'))
-                    writer.writerow(csvRow)
-finally:
-    csvFile.close()
+                    print(a.string.strip())
+                    print('http:/' + a.get('href'))
+            print("\n")
+            i += 1 
+            if i >= 5:
+                return
+
+import os
+if __name__ == "__main__":
+    miyadaiOshirase()
